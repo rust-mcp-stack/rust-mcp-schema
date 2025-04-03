10aca90c78467cf68ede3e06ea1f49236244fcf2
Ali Hashemi <14126952+hashemix@users.noreply.github.com>
2025-04-03 20:00:47 -0300

    feat!: add support for schema version 2025-03-26 (#62)
    * feat: support 2025-03-26
    
    * Update README.md

--- src/generated_schema.rs
+++ src/generated_schema.rs
@@ -1,25 +1,25 @@
-/// Schema Version : Draft
-#[cfg(feature = "draft")]
-#[path = "generated_schema/draft/mcp_schema.rs"]
-mod schema_draft;
-#[cfg(feature = "draft")]
-pub use schema_draft::*;
-
-#[cfg(all(feature = "schema_utils", feature = "draft"))]
-#[path = "generated_schema/draft/schema_utils.rs"]
-pub mod schema_utils;
-
 /// Schema Version : 2024_11_05
 #[cfg(feature = "2024_11_05")]
-#[cfg(not(feature = "draft"))]
 #[path = "generated_schema/2024_11_05/mcp_schema.rs"]
 mod schema_2024_11_05;
-
 #[cfg(feature = "2024_11_05")]
-#[cfg(not(feature = "draft"))]
 pub use schema_2024_11_05::*;
 
 #[cfg(all(feature = "schema_utils", feature = "2024_11_05"))]
-#[cfg(not(feature = "draft"))]
 #[path = "generated_schema/2024_11_05/schema_utils.rs"]
+pub mod schema_utils;
+
+/// Schema Version : 2025_03_26
+#[cfg(feature = "2025_03_26")]
+#[cfg(not(feature = "2024_11_05"))]
+#[path = "generated_schema/2025_03_26/mcp_schema.rs"]
+mod schema_2025_03_26;
+
+#[cfg(feature = "2025_03_26")]
+#[cfg(not(feature = "2024_11_05"))]
+pub use schema_2025_03_26::*;
+
+#[cfg(all(feature = "schema_utils", feature = "2025_03_26"))]
+#[cfg(not(feature = "2024_11_05"))]
+#[path = "generated_schema/2025_03_26/schema_utils.rs"]
 pub mod schema_utils;
