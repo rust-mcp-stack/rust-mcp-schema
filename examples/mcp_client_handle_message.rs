90f3939dc9ecd82b0e853350b89adcf8f71c9cae
Ali Hashemi <programmer.ice@gmail.com>
2025-12-13 17:19:02 -0400

    chore: typo

--- examples/mcp_client_handle_message.rs
+++ examples/mcp_client_handle_message.rs
@@ -28,13 +28,13 @@ const SAMPLE_PAYLOAD: &str = r#"
 
 fn main() {
     if let Err(error) = handle_message(SAMPLE_PAYLOAD) {
-        eprintln!("Error occured: {:?}", error);
+        eprintln!("Error occurred: {:?}", error);
     }
 }
 
 
-   
-// Determine if the message is an Error        
+
+// Determine if the message is an Error
 fn handle_message(message_payload: &str) -> std::result::Result<(), AppError> {
     // Deserialize message into ServerMessage.
     //ServerMessage is a message sent by an MCP Server to an MCP Client.
@@ -43,19 +43,19 @@ fn handle_message(message_payload: &str) -> std::result::Result<(), AppError> {
         // Determine if the message is a Request
         ServerMessage::Request(request) => match request {
             ServerJsonrpcRequest::PingRequest(ping_request) => println!("Ping request received: {:?}", ping_request),
-            ServerJsonrpcRequest::GetTaskRequest(get_task_request) => println!("GetTaskRequest request received: {:?}", get_task_request),            
+            ServerJsonrpcRequest::GetTaskRequest(get_task_request) => println!("GetTaskRequest request received: {:?}", get_task_request),
             ServerJsonrpcRequest::GetTaskPayloadRequest(get_task_payload_request) => println!("GetTaskPayloadRequest request received: {:?}", get_task_payload_request),
             ServerJsonrpcRequest::CancelTaskRequest(cancel_task_request) => println!("CancelTaskRequest request received: {:?}", cancel_task_request),
             ServerJsonrpcRequest::ListTasksRequest(list_tasks_request) => println!("ListTasksRequest request received: {:?}", list_tasks_request),
             ServerJsonrpcRequest::CreateMessageRequest(create_message_request) => println!("CreateMessageRequest request received: {:?}", create_message_request),
             ServerJsonrpcRequest::ListRootsRequest(list_roots_request) => println!("ListRootsRequest request received: {:?}", list_roots_request),
-            ServerJsonrpcRequest::ElicitRequest(elicit_request) => println!("ElicitRequest request received: {:?}", elicit_request),            
+            ServerJsonrpcRequest::ElicitRequest(elicit_request) => println!("ElicitRequest request received: {:?}", elicit_request),
             ServerJsonrpcRequest::CustomRequest(jsonrpc_request) => println!("CustomRequest request received: {:?}", jsonrpc_request),
         },
         // Determine if the message is a Notification
         ServerMessage::Notification(notification) => match notification {
-            ServerJsonrpcNotification::CancelledNotification(cancelled_notification) => println!("CancelledNotification notification received: {:?}", cancelled_notification),            
-            ServerJsonrpcNotification::ProgressNotification(progress_notification) => println!("ProgressNotification notification received: {:?}", progress_notification),    
+            ServerJsonrpcNotification::CancelledNotification(cancelled_notification) => println!("CancelledNotification notification received: {:?}", cancelled_notification),
+            ServerJsonrpcNotification::ProgressNotification(progress_notification) => println!("ProgressNotification notification received: {:?}", progress_notification),
             ServerJsonrpcNotification::ResourceListChangedNotification(resource_list_changed_notification) => println!("ResourceListChangedNotification notification received: {:?}",resource_list_changed_notification),
             ServerJsonrpcNotification::ResourceUpdatedNotification(resource_updated_notification) => println!("ResourceUpdatedNotification notification received: {:?}",resource_updated_notification),
             ServerJsonrpcNotification::PromptListChangedNotification(prompt_list_changed_notification) => println!("PromptListChangedNotification notification received: {:?}",prompt_list_changed_notification),
@@ -65,18 +65,18 @@ fn handle_message(message_payload: &str) -> std::result::Result<(), AppError> {
             ServerJsonrpcNotification::ElicitationCompleteNotification(elicitation_complete_notification) => println!("ElicitationCompleteNotification notification received: {:?}",elicitation_complete_notification),
             ServerJsonrpcNotification::CustomNotification(jsonrpc_notification) => println!("CustomNotification notification received: {:?}", jsonrpc_notification)
         },
-    // Determine if the message is a Response 
+    // Determine if the message is a Response
     ServerMessage::Response(response) => match &response.result {
-            ServerResult::InitializeResult(_initialize_result) =>  println!("InitializeResult response received: {:?}", response),            
-            ServerResult::ListResourcesResult(_list_resources_result) =>  println!("ListResourcesResult response received: {:?}", response),            
-            ServerResult::ListResourceTemplatesResult(_list_resource_templates_result) =>  println!("ListResourceTemplatesResult response received: {:?}", response),            
-            ServerResult::ReadResourceResult(_read_resource_result) =>  println!("ReadResourceResult response received: {:?}", response),            
-            ServerResult::ListPromptsResult(_list_prompts_result) =>  println!("ListPromptsResult response received: {:?}", response),        
+            ServerResult::InitializeResult(_initialize_result) =>  println!("InitializeResult response received: {:?}", response),
+            ServerResult::ListResourcesResult(_list_resources_result) =>  println!("ListResourcesResult response received: {:?}", response),
+            ServerResult::ListResourceTemplatesResult(_list_resource_templates_result) =>  println!("ListResourceTemplatesResult response received: {:?}", response),
+            ServerResult::ReadResourceResult(_read_resource_result) =>  println!("ReadResourceResult response received: {:?}", response),
+            ServerResult::ListPromptsResult(_list_prompts_result) =>  println!("ListPromptsResult response received: {:?}", response),
             ServerResult::GetPromptResult(_get_prompt_result) => println!("GetPromptResult response received: {:?}", response),
             ServerResult::ListToolsResult(_list_tools_result) => println!("ListToolsResult response received: {:?}", response),
             ServerResult::CallToolResult(_call_tool_result) => println!("CallToolResult response received: {:?}", response),
             ServerResult::GetTaskResult(_get_task_result) => println!("GetTaskResult response received: {:?}", response),
-            ServerResult::CancelTaskResult(_cancel_task_result) => println!("CancelTaskResult response received: {:?}", response),            
+            ServerResult::CancelTaskResult(_cancel_task_result) => println!("CancelTaskResult response received: {:?}", response),
             ServerResult::ListTasksResult(_list_tasks_result) => println!("ListTasksResult response received: {:?}", response),
             ServerResult::CompleteResult(_complete_result) => println!("CompleteResult response received: {:?}", response),
             ServerResult::Result(_generic_result) => println!("Generic Result response received: {:?}", response),
--- examples/mcp_server_handle_message.rs
+++ examples/mcp_server_handle_message.rs
@@ -30,7 +30,7 @@ const SAMPLE_PAYLOAD: &str = r#"
 fn main() {
     #[cfg(feature = "latest")]
     if let Err(error) = handle_message(SAMPLE_PAYLOAD) {
-        eprintln!("Error occured: {:?}", error);
+        eprintln!("Error occurred: {:?}", error);
     }
 }
 
@@ -44,103 +44,44 @@ fn handle_message(message_payload: &str) -> std::result::Result<(), AppError> {
     match mcp_message {
         // Determine if the message is a Request
         ClientMessage::Request(request) => match request {
-            ClientJsonrpcRequest::InitializeRequest(initialize_request) => {
-                println!("InitializeRequest request received: {:?}", initialize_request)
-            }
+            ClientJsonrpcRequest::InitializeRequest(initialize_request) => println!("InitializeRequest request received: {:?}", initialize_request),
             ClientJsonrpcRequest::PingRequest(ping_request) => println!("PingRequest request received: {:?}", ping_request),
-            ClientJsonrpcRequest::ListResourcesRequest(list_resources_request) => {
-                println!("ListResourcesRequest request received: {:?}", list_resources_request)
-            }
-            ClientJsonrpcRequest::ListResourceTemplatesRequest(list_resource_templates_request) => println!(
-                "ListResourceTemplatesRequest request received: {:?}",
-                list_resource_templates_request
-            ),
-            ClientJsonrpcRequest::ReadResourceRequest(read_resource_request) => {
-                println!("ReadResourceRequest request received: {:?}", read_resource_request)
-            }
-            ClientJsonrpcRequest::SubscribeRequest(subscribe_request) => {
-                println!("SubscribeRequest request received: {:?}", subscribe_request)
-            }
-            ClientJsonrpcRequest::UnsubscribeRequest(unsubscribe_request) => {
-                println!("UnsubscribeRequest request received: {:?}", unsubscribe_request)
-            }
-            ClientJsonrpcRequest::ListPromptsRequest(list_prompts_request) => {
-                println!("ListPromptsRequest request received: {:?}", list_prompts_request)
-            }
-            ClientJsonrpcRequest::GetPromptRequest(get_prompt_request) => {
-                println!("GetPromptRequest request received: {:?}", get_prompt_request)
-            }
-            ClientJsonrpcRequest::ListToolsRequest(list_tools_request) => {
-                println!("ListToolsRequest request received: {:?}", list_tools_request)
-            }
-            ClientJsonrpcRequest::CallToolRequest(call_tool_request) => {
-                println!("CallToolRequest request received: {:?}", call_tool_request)
-            }
-            ClientJsonrpcRequest::GetTaskRequest(get_task_request) => {
-                println!("GetTaskRequest request received: {:?}", get_task_request)
-            }
-            ClientJsonrpcRequest::GetTaskPayloadRequest(get_task_payload_request) => {
-                println!("GetTaskPayloadRequest request received: {:?}", get_task_payload_request)
-            }
-            ClientJsonrpcRequest::CancelTaskRequest(cancel_task_request) => {
-                println!("CancelTaskRequest request received: {:?}", cancel_task_request)
-            }
-            ClientJsonrpcRequest::ListTasksRequest(list_tasks_request) => {
-                println!("ListTasksRequest request received: {:?}", list_tasks_request)
-            }
-            ClientJsonrpcRequest::SetLevelRequest(set_level_request) => {
-                println!("SetLevelRequest request received: {:?}", set_level_request)
-            }
-            ClientJsonrpcRequest::CompleteRequest(complete_request) => {
-                println!("CompleteRequest request received: {:?}", complete_request)
-            }
-            ClientJsonrpcRequest::CustomRequest(jsonrpc_request) => {
-                println!("CustomRequest request received: {:?}", jsonrpc_request)
-            }
+            ClientJsonrpcRequest::ListResourcesRequest(list_resources_request) => println!("ListResourcesRequest request received: {:?}", list_resources_request),
+            ClientJsonrpcRequest::ListResourceTemplatesRequest(list_resource_templates_request) => println!("ListResourceTemplatesRequest request received: {:?}",list_resource_templates_request),
+            ClientJsonrpcRequest::ReadResourceRequest(read_resource_request) => println!("ReadResourceRequest request received: {:?}", read_resource_request),
+            ClientJsonrpcRequest::SubscribeRequest(subscribe_request) => println!("SubscribeRequest request received: {:?}", subscribe_request),
+            ClientJsonrpcRequest::UnsubscribeRequest(unsubscribe_request) => println!("UnsubscribeRequest request received: {:?}", unsubscribe_request),
+            ClientJsonrpcRequest::ListPromptsRequest(list_prompts_request) => println!("ListPromptsRequest request received: {:?}", list_prompts_request),
+            ClientJsonrpcRequest::GetPromptRequest(get_prompt_request) => println!("GetPromptRequest request received: {:?}", get_prompt_request),
+            ClientJsonrpcRequest::ListToolsRequest(list_tools_request) => println!("ListToolsRequest request received: {:?}", list_tools_request),
+            ClientJsonrpcRequest::CallToolRequest(call_tool_request) => println!("CallToolRequest request received: {:?}", call_tool_request),
+            ClientJsonrpcRequest::GetTaskRequest(get_task_request) => println!("GetTaskRequest request received: {:?}", get_task_request),
+            ClientJsonrpcRequest::GetTaskPayloadRequest(get_task_payload_request) => println!("GetTaskPayloadRequest request received: {:?}", get_task_payload_request),
+            ClientJsonrpcRequest::CancelTaskRequest(cancel_task_request) => println!("CancelTaskRequest request received: {:?}", cancel_task_request),
+            ClientJsonrpcRequest::ListTasksRequest(list_tasks_request) => println!("ListTasksRequest request received: {:?}", list_tasks_request),
+            ClientJsonrpcRequest::SetLevelRequest(set_level_request) => println!("SetLevelRequest request received: {:?}", set_level_request),
+            ClientJsonrpcRequest::CompleteRequest(complete_request) => println!("CompleteRequest request received: {:?}", complete_request),
+            ClientJsonrpcRequest::CustomRequest(jsonrpc_request) => println!("CustomRequest request received: {:?}", jsonrpc_request),
         },
         // Determine if the message is a Notification
         ClientMessage::Notification(notification) => match notification {
-            ClientJsonrpcNotification::CancelledNotification(cancelled_notification) => {
-                println!("CancelledNotification notification received: {:?}", cancelled_notification)
-            }
-            ClientJsonrpcNotification::InitializedNotification(initialized_notification) => println!(
-                "InitializedNotification notification received: {:?}",
-                initialized_notification
-            ),
-            ClientJsonrpcNotification::ProgressNotification(progress_notification) => {
-                println!("ProgressNotification notification received: {:?}", progress_notification)
-            }
-            ClientJsonrpcNotification::TaskStatusNotification(task_status_notification) => {
-                println!("TaskStatusNotification notification received: {:?}", task_status_notification)
-            }
-            ClientJsonrpcNotification::RootsListChangedNotification(roots_list_changed_notification) => println!(
-                "RootsListChangedNotification notification received: {:?}",
-                roots_list_changed_notification
-            ),
-            ClientJsonrpcNotification::CustomNotification(jsonrpc_notification) => {
-                println!("CustomNotification notification received: {:?}", jsonrpc_notification)
-            }
+            ClientJsonrpcNotification::CancelledNotification(cancelled_notification) => println!("CancelledNotification notification received: {:?}", cancelled_notification),
+            ClientJsonrpcNotification::InitializedNotification(initialized_notification) => println!("InitializedNotification notification received: {:?}",initialized_notification),
+            ClientJsonrpcNotification::ProgressNotification(progress_notification) => println!("ProgressNotification notification received: {:?}", progress_notification),
+            ClientJsonrpcNotification::TaskStatusNotification(task_status_notification) => println!("TaskStatusNotification notification received: {:?}", task_status_notification),
+            ClientJsonrpcNotification::RootsListChangedNotification(roots_list_changed_notification) => println!("RootsListChangedNotification notification received: {:?}",roots_list_changed_notification),
+            ClientJsonrpcNotification::CustomNotification(jsonrpc_notification) => println!("CustomNotification notification received: {:?}", jsonrpc_notification),
         },
         // Determine if the message is a Response
         ClientMessage::Response(response) => match &response.result {
             ClientResult::GetTaskResult(_get_task_result) => println!("GetTaskResult  response received: {:?}", response),
-            ClientResult::CancelTaskResult(_cancel_task_result) => {
-                println!("CancelTaskResult  response received: {:?}", response)
-            }
-            ClientResult::ListTasksResult(_list_tasks_result) => {
-                println!("ListTasksResult  response received: {:?}", response)
-            }
-            ClientResult::CreateMessageResult(_create_message_result) => {
-                println!("CreateMessageResult  response received: {:?}", response)
-            }
-            ClientResult::ListRootsResult(_list_roots_result) => {
-                println!("ListRootsResult  response received: {:?}", response)
-            }
+            ClientResult::CancelTaskResult(_cancel_task_result) => println!("CancelTaskResult  response received: {:?}", response),
+            ClientResult::ListTasksResult(_list_tasks_result) => println!("ListTasksResult  response received: {:?}", response),
+            ClientResult::CreateMessageResult(_create_message_result) => println!("CreateMessageResult  response received: {:?}", response),
+            ClientResult::ListRootsResult(_list_roots_result) => println!("ListRootsResult  response received: {:?}", response),
             ClientResult::ElicitResult(_elicit_result) => println!("ElicitResult  response received: {:?}", response),
             ClientResult::Result(_generic_result) => println!("Generic Result response received: {:?}", response),
-            ClientResult::GetTaskPayloadResult(_generic_result) => {
-                println!("Generic Result response received: {:?}", response)
-            }
+            ClientResult::GetTaskPayloadResult(_generic_result) => println!("Generic Result response received: {:?}", response),
         },
         // Determine if the message is an Error
         ClientMessage::Error(error_response) => {
